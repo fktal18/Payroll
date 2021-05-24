6fb821602fe4fc2cceec4b477538591ef438f6db
fktal18 <fenl6547395@163.com>
2021-05-24 21:36:46 +0800

    新建一个测试类：添加雇员类

---
+++ src/payroll/test/AddEmployeeTest.java
@@ -0,0 +1,14 @@
+package payroll.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AddEmployeeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
