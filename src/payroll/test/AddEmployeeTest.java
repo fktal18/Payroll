7c6627fa0d5d532e00f25eb6f629f8196b99c085
fktal18 <fenl6547395@163.com>
2021-05-25 13:19:08 +0800

    新建类：PayrollDatabase(数据库类)

--- src/payroll/test/AddEmployeeTest.java
+++ src/payroll/test/AddEmployeeTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import payroll.Employee;
+import payroll.PayrollDatabase;
 import payroll.Transaction;
 import payroll.trans.AddHourlyEmployeeTransaction;
 
