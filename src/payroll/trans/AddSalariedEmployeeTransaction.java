30be528d47e9fb50bfe41221a9a102b1c2f352d6
fktal18 <fenl6547395@163.com>
2021-05-25 15:19:53 +0800

    修改类：AddSalariedEmployeeTransaction新建构造方法，以及相对应的属性

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -4,6 +4,18 @@ import payroll.Transaction;
 
 public class AddSalariedEmployeeTransaction implements Transaction {
 
+	private int empId;
+	private String name;
+	private String address;
+	private double salary;
+
+	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
+		this.empId = empId;
+		this.name = name;
+		this.address = address;
+		this.salary = salary;
+	}
+
 	@Override
 	public void execute() {
 
