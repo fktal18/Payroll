0f83a9251cb58c7cbf690fd1f598a8990200e911
fktal18 <fenl6547395@163.com>
2021-05-25 16:57:50 +0800

    修改类:DeleteEmployeeTransaction新建构造方法以及对应属性

--- src/payroll/trans/DeleteEmployeeTransaction.java
+++ src/payroll/trans/DeleteEmployeeTransaction.java
@@ -4,6 +4,12 @@ import payroll.Transaction;
 
 public class DeleteEmployeeTransaction implements Transaction {
 
+	private int empId;
+
+	public DeleteEmployeeTransaction(int empId) {
+		this.empId = empId;
+	}
+
 	@Override
 	public void execute() {
 
