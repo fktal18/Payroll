e0d94d4d27e875c6e35102758b014d2fe123acf9
fktal18 <fenl6547395@163.com>
2021-05-25 16:37:11 +0800

    修改类：CommissionedClassification添加方法getSalary,getCommissionRate

--- src/payroll/classification/CommissionedClassification.java
+++ src/payroll/classification/CommissionedClassification.java
@@ -4,4 +4,12 @@ import payroll.PaymentClassification;
 
 public class CommissionedClassification extends PaymentClassification {
 
+	public double getSalary() {
+		return 0;
+	}
+
+	public double getCommissionRate() {
+		return 0;
+	}
+
 }
