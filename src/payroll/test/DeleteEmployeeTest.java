df13e03b1eb2c0ebfb22db9ee9a5a521f12291d2
fktal18 <fenl6547395@163.com>
2021-05-25 16:45:14 +0800

    编写注释，编写删除雇员方法

--- src/payroll/test/DeleteEmployeeTest.java
+++ src/payroll/test/DeleteEmployeeTest.java
@@ -6,9 +6,17 @@ import org.junit.jupiter.api.Test;
 
 class DeleteEmployeeTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	//用例2  删除雇员。
+	//使用DelEmp操作来删除雇员。操作形式：
+	//DelEmp EmpId
+	//当执行该操作时，会删除对应的雇员记录。
+	//异常情况1：无效或者未知的EmpId。
+	//如果EmpId字段不具有正确的结构，或者它没有引用到一条有效的雇员记录，会打印一条错误
+	//消息，不进行其他处理。
 
+	@Test
+	public void DeleteEmployeeTest() {
+		//先插入一个雇员，然后删除
+		
+	}
 }
