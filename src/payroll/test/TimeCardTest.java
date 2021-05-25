28e5b2f9d2b76f933d95348ab645b8beb975bec6
fktal18 <fenl6547395@163.com>
2021-05-25 17:31:46 +0800

    编写方法名称

--- src/payroll/test/TimeCardTest.java
+++ src/payroll/test/TimeCardTest.java
@@ -6,9 +6,30 @@ import org.junit.jupiter.api.Test;
 
 class TimeCardTest {
 
+	//登记时间卡（一个时间卡，钟点工）
+	//TimeCard EmpId date hours
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testOneTimeCardToHourlyEmplouyee() {
+		
 	}
-
+	//登记时间卡（两个时间卡，钟点工）
+	@Test
+	public void testTwoTimeCardToHourlyEmplouyee() {
+		
+	}
+	//登记时间咯（一个时间卡，月薪雇员，抛出异常）
+	@Test
+	public void testTimeCardToSalariedEmplouyee() {
+		
+	}
+	//登记时间卡（一个时间卡，月薪加提成雇员，抛出异常）
+	@Test
+	public void testTimeCardToCommissionedEmplouyee() {
+		
+	}
+	//登记时间卡（一个时间卡，不存在的雇员，抛出异常）
+	@Test
+	public void testTimeCardToNoSuchEmployee() {
+		
+	}
 }
