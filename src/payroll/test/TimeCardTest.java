c2ebd52af7b25227a7116fb6bda9208095d2ff63
fktal18 <fenl6547395@163.com>
2021-05-25 17:18:09 +0800

    新建打卡测试

---
+++ src/payroll/test/TimeCardTest.java
@@ -0,0 +1,14 @@
+package payroll.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TimeCardTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
