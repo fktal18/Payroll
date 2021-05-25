4464e2d90ea8b49acd7ba21def5a5ec974fc78a4
fktal18 <fenl6547395@163.com>
2021-05-25 13:15:35 +0800

    修改类:AddHourlyEmployeeTransaction的构造方法，以及创建涉及的属性

--- src/payroll/trans/AddHourlyEmployeeTransaction.java
+++ src/payroll/trans/AddHourlyEmployeeTransaction.java
@@ -4,4 +4,16 @@ import payroll.Transaction;
 
 public class AddHourlyEmployeeTransaction implements Transaction {
 
+	private int empId;
+	private String name;
+	private String address;
+	private double hourlyRate;
+
+	public AddHourlyEmployeeTransaction(int empId, String name, String address, double hourlyRate) {
+		this.empId = empId;
+		this.name = name;
+		this.address = address;
+		this.hourlyRate = hourlyRate;
+	}
+
 }
