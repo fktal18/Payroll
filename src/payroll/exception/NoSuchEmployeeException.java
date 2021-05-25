fdf2e33aba38f6726fec563872cee6dec48ed8ee
fktal18 <fenl6547395@163.com>
2021-05-25 17:04:03 +0800

    修改类：NoSuchEmployeeException添加构造方法

--- src/payroll/exception/NoSuchEmployeeException.java
+++ src/payroll/exception/NoSuchEmployeeException.java
@@ -2,9 +2,9 @@ package payroll.exception;
 
 public class NoSuchEmployeeException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
+	public NoSuchEmployeeException(String message) {
+		super(message);
+	}
 }
