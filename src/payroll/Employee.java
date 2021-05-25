57388831705481712129b1ed56084e01d8b1b61c
fktal18 <fenl6547395@163.com>
2021-05-25 13:23:45 +0800

    修改类：为Employee(雇员)类添加三个属性，以及相应的构造方法

--- src/payroll/Employee.java
+++ src/payroll/Employee.java
@@ -1,5 +1,36 @@
 package payroll;
 
 public class Employee {
-
+	private Integer empId;
+	private String name;
+	private String address;
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Employee(Integer empId, String name, String address) {
+		super();
+		this.empId = empId;
+		this.name = name;
+		this.address = address;
+	}
+	public Employee() {
+		super();
+	}
+	
+	
 }
