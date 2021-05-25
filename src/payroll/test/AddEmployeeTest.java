0832129c3d280e2c9afa9803f0e834d54f555027
fktal18 <fenl6547395@163.com>
2021-05-25 13:14:55 +0800

    新建类：AddHourlyEmployeeTransaction(添加小时工雇员)

--- src/payroll/test/AddEmployeeTest.java
+++ src/payroll/test/AddEmployeeTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import payroll.Transaction;
+import payroll.trans.AddHourlyEmployeeTransaction;
 
 class AddEmployeeTest {
 	//用例1：增加雇员。
