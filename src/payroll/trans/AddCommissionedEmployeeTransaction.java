37a4eecdbd160ad81090eb8097989c50bbf9e4fa
fktal18 <fenl6547395@163.com>
2021-05-25 16:35:19 +0800

    修改类：AddCommissionedEmployeeTransaction，添加构造方法以及对应所需属性

--- src/payroll/trans/AddCommissionedEmployeeTransaction.java
+++ src/payroll/trans/AddCommissionedEmployeeTransaction.java
@@ -4,6 +4,17 @@ import payroll.PaymentClassification;
 
 public class AddCommissionedEmployeeTransaction  extends AddEmployeeTransaction{
 
+	private double salary;
+	private double commissionRate;
+
+	public AddCommissionedEmployeeTransaction(int empId, String name, String address, double salary,
+			double commissionRate) {
+		super(empId, name, address);
+		this.salary = salary;
+		this.commissionRate = commissionRate;
+		
+	}
+
 	@Override
 	protected PaymentClassification getPaymentClassification() {
 		return null;
