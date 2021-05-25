874056d64f7502aa162f5d9ce81285c4114f0755
fktal18 <fenl6547395@163.com>
2021-05-25 16:44:09 +0800

    新建删除对象测试类

---
+++ src/payroll/test/DeleteEmployeeTest.java
@@ -0,0 +1,14 @@
+package payroll.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DeleteEmployeeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
