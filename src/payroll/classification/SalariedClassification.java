17075fe41362407168b8280068a1b0f040bdf5c2
fktal18 <fenl6547395@163.com>
2021-05-25 15:35:52 +0800

    修改类:SalariedClassification实现构造方法，并修改getSalary()方法

--- src/payroll/classification/SalariedClassification.java
+++ src/payroll/classification/SalariedClassification.java
@@ -4,6 +4,9 @@ import payroll.PaymentClassification;
 
 public class SalariedClassification extends PaymentClassification {
 	private double salary;
+	public SalariedClassification(double salary) {
+		this.salary=salary;
+	}
 	public double getSalary() {
 		return salary;
 	}
