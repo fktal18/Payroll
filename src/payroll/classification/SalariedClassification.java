6a4a98342480ef89669966c5f3b9f120ccff5e8b
fktal18 <fenl6547395@163.com>
2021-05-25 15:30:42 +0800

    修改类：SalariedClassification的属性，方法

--- src/payroll/classification/SalariedClassification.java
+++ src/payroll/classification/SalariedClassification.java
@@ -3,5 +3,9 @@ package payroll.classification;
 import payroll.PaymentClassification;
 
 public class SalariedClassification extends PaymentClassification {
+	private double salary;
+	public double getSalary() {
+		return salary;
+	}
 
 }
