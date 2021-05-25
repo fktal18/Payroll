72831dc31ced28ca7b030a1693d3e698af5114df
fktal18 <fenl6547395@163.com>
2021-05-25 13:30:13 +0800

    添加类：PaymentClassification(雇员工资分类)

--- src/payroll/test/AddEmployeeTest.java
+++ src/payroll/test/AddEmployeeTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import payroll.Employee;
+import payroll.PaymentClassification;
 import payroll.PayrollDatabase;
 import payroll.Transaction;
 import payroll.trans.AddHourlyEmployeeTransaction;
